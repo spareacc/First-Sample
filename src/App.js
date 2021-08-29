177f9e3c35cf4e14f73b482bb809f9924339e501
spareacc <68348405+spareacc@users.noreply.github.com>
2021-08-30 00:50:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Chaitanya Here!</h1>
+      <h2>This is My Personal Site Welcome!</h2>
+    </div>
+  );
+}
